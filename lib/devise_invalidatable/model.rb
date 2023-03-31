61b433b3212567cbd1c43f8de23dd2a427cb6923
Sahil Hussain <30535666+sahilas@users.noreply.github.com>
2023-03-31 10:44:44 +0530

    Update model.rb

--- lib/devise_invalidatable/model.rb
+++ lib/devise_invalidatable/model.rb
@@ -30,7 +30,7 @@ module Devise
       end
 
       def purge_old_sessions
-        user_sessions.order('created_at desc').offset(10).destroy_all
+        user_sessions.order('created_at desc').offset(1).destroy_all
       end
 
     end
